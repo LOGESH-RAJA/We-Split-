923b58d715166c28967108d41befc0406ec55e5f
Logesh R <logesh822081@gmail.com>
2024-10-30 11:19:27 +0530

    Initial Commit

---
+++ We.splitapp/We_splitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_splitappApp.swift
+//  We.splitapp
+//
+//  Created by Logesh Raja on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct We_splitappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ We.splitappTests/We_splitappTests.swift
@@ -0,0 +1,17 @@
+//
+//  We_splitappTests.swift
+//  We.splitappTests
+//
+//  Created by Logesh Raja on 10/30/24.
+//
+
+import Testing
+@testable import We_splitapp
+
+struct We_splitappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
